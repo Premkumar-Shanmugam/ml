3c34392d46d00a1d2a90099194750acb7191295e
Premkumar Shanmugam <premkumar.shanmugam@gmail.com>
2020-03-30 09:06:25 +0530

    Add Un-supervised Learning Project

---
+++ IdentifyCustomerSegments/helper_functions.py
@@ -0,0 +1,63 @@
+###########################################
+# Suppress matplotlib user warnings
+# Necessary for newer version of matplotlib
+import warnings
+warnings.filterwarnings("ignore", category = UserWarning, module = "matplotlib")
+#
+# Display inline matplotlib plots with IPython
+from IPython import get_ipython
+get_ipython().run_line_magic('matplotlib', 'inline')
+###########################################
+
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+import matplotlib.pyplot as plt
+
+def do_pca(n_components, data):
+    '''
+    Transforms data using PCA to create n_components, and provides back the results of the
+    transformation.
+
+    INPUT: n_components - int - the number of principal components to create
+           data - the data you would like to transform
+
+    OUTPUT: pca - the pca object created after fitting the data
+            X_pca - the transformed X matrix with new number of components
+    '''
+    X = StandardScaler().fit_transform(data)
+    pca = PCA(n_components)
+    X_pca = pca.fit_transform(X)
+    return pca, X_pca
+
+
+def scree_plot(pca):
+    '''
+    Creates a scree plot associated with the principal components 
+    
+    INPUT: pca - the result of instantian of PCA in scikit learn
+            
+    OUTPUT:
+            None
+    '''
+    num_components=len(pca.explained_variance_ratio_)
+    ind = np.arange(num_components)
+    vals = pca.explained_variance_ratio_
+ 
+    plt.figure(figsize=(20, 6))
+    ax = plt.subplot(111)
+    cumvals = np.cumsum(vals)
+    ax.bar(ind, vals)
+    ax.plot(ind, cumvals)
+    for i in range(num_components):
+        ax.annotate(r"%s%%" % ((str(vals[i]*100)[:4])), (ind[i]+0.2, vals[i]), va="bottom", ha="center", fontsize=12)
+ 
+    ax.xaxis.set_tick_params(width=0)
+    ax.yaxis.set_tick_params(width=2, length=12)
+ 
+    ax.set_xlabel("Principal Component")
+    ax.set_ylabel("Variance Explained (%)")
+    plt.title('Explained Variance Per Principal Component')
+
--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Repository for Machine Learning projects
 
-1. [Finding Donors](finding_donors) - Create a Supervised Learning model for CharityML that predicts whether an individual makes more than $50,000
+1. Project-1: Supervised Learning | [Finding Donors](finding_donors) - Create a Supervised Learning model for CharityML that predicts whether an individual makes more than $50,000
 2. [Gradient Descent](GradientDescent) - Lab exercise to implement the Gradient Descent Algorithm
 3. [Student Admissions](StudentAdmissions) - Lab exercise to implement a 2-layer neural network that predicts student admissions based on Test, Grade and Rank
 4. [Deep Learning Excercises](DeepLearningExercises) - Lab exercises on Deep Learning using PyTorch
-5. [Image Classifier](ImageClassifier) - Create a Deep Learning model that predicts the name of the flower in an image from among the 102 classes of flowers
+5. Project-2: Deep Learning | [Image Classifier](ImageClassifier) - Create a Deep Learning model that predicts the name of the flower in an image from among the 102 classes of flowers
 6. [Un-supervised Learning Exercises](UnsupervisedLearningExercises) - Lab exercises on Clustering
+7. Project-3: Un-supervised Learning | [Identify Customer Segments](IdentifyCustomerSegments) - Identify customer segments of a mail-order sales company
